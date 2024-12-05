61c8d1405f4bf974d2fdd39570b0eb119d320ad9
svig <sumit.vig@gmail.com>
2024-12-05 18:23:57 -0500

    Update vite.config.js
    enable npm run dev on remote server

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
 
 export default defineConfig({
+    server: {
+        host: 'ws2.domaindriven.net',
+        port: 3001,
+        cors: true,
+        https: {
+            cert: fs.readFileSync('/home/ws2/ssl.cert'),
+            key: fs.readFileSync('/home/ws2/ssl.key'),
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
